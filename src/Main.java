ca61e76a3fd472b358ca0701bd3662a7ba00381f
paula <paula.anna.wesolowska@gmail.com>
2021-12-19 09:13:24 +0100

    added first task

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,104 @@ public class Main {
         */
 
         /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
-        * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
+         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
-    }
-}
+
+        // zadanie 1a
+        enum LiczbyEnum {
+            JEDEN, DWA, TRZY, CZTERY, PIĘĆ, SZEŚĆ;
+        }
+        // zadanie 1b
+        enum StatusEnum {
+            KONTYNUUJEMY, KONIEC
+        }
+        // zadanie 1c
+        boolean wielkosc = true;
+        while (wielkosc) {
+            System.out.println("Pętla działa");
+        }
+        // zadanie 1d
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        while (wielkosc){
+            int number = scan.nextInt();
+            System.out.println(number);
+        }
+        // zadanie 1e
+        System.out.println("Podaj liczbę");
+        while (true) {
+            int s1 = scan.nextInt();
+            if (s1 == 0) {
+                break;
+            } else {
+                System.out.println(s1);
+            }
+        }
+
+        // zadanie 1e
+        System.out.println("Podaj liczbę");
+        while (true) {
+            int s1 = scan.nextInt();
+            if (s1 == 0) {
+                break;
+            } else {
+                System.out.println(s1);
+            }
+        }
+        // zadanie 1f
+        System.out.println("Podaj liczbę");
+        int nr;
+        do {
+            nr = scan.nextInt();
+            switch (nr) {
+                case 0:
+                    System.out.println("Koniec działania programu");
+                    break;
+                case 1:
+                    System.out.println("Jeden");
+                    break;
+                case 2:
+                    System.out.println("Dwa");
+                    break;
+                case 3:
+                    System.out.println("Trzy");
+                    break;
+                case 4:
+                    System.out.println("Cztery");
+                    break;
+                case 5:
+                    System.out.println("Pięć");
+                    break;
+                case 6:
+                    System.out.println("Sześć");
+                    break;
+                case 7:
+                    System.out.println("Siedem");
+                    break;
+                case 8:
+                    System.out.println("Osiem");
+                    break;
+                case 9:
+                    System.out.println("Dziewięć");
+                    break;
+                case 10:
+                    System.out.println("Dziesięć");
+                    break;
+                default:
+                    System.out.println("Nie ma takiej liczby");
+                    break;
+            }
+        }while(nr != 0);
+    // zadanie 1g
+        System.out.println("Podaj liczbę");
+        while(true) {
+            int nr1 = scan.nextInt();
+            if (nr1 == 0) {
+                System.out.println(StatusEnum.KONIEC.toString());
+                break;
+            } else {
+                System.out.println(StatusEnum.KONTYNUUJEMY.toString());
+            }
+        }
+
+    }}

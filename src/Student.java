637b203100f5dcbf2bffd9eb76987cb42b7b288f
paula <paula.anna.wesolowska@gmail.com>
2021-12-19 10:25:58 +0100

    added second task

---
+++ src/Student.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public int indexNumber;
+    public boolean classified;
+
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+
+        s1.firstName = "Kasia";
+        s1.lastName = "Malinowska";
+        s1.indexNumber = 1234;
+        s1.classified = true;
+
+        s2.firstName = "Jan";
+        s2.lastName = "Kalinowski";
+        s2.indexNumber = 6667;
+        s2.classified = true;
+
+        s3.firstName = "Marcin";
+        s3.lastName = "Nowak";
+        s3.indexNumber = 1222;
+        s3.classified = false;
+
+        Student[] studentObjects = new Student[3];
+        studentObjects[0] = s1;
+        studentObjects[1] = s2;
+        studentObjects[2] = s3;
+
+        for (Student s : studentObjects){
+            System.out.println("Imię: " + s.firstName + ", nazwisko: " + s.lastName + ", nr indeksu: " + s.indexNumber + ", klasyfikowany: " + s.classified );
+        }
+    }
+}
+
+
+//    List<Pojazd> pojazdList = new ArrayList<>();
+//
+//    /* Deklaracja z inicjalizacją pola 'pojazd' typem Pojazd */
+//    Pojazd pojazd = new Pojazd();
+//        pojazd.kolor = KolorEnum.NIEBIESKI;
+//                pojazd.iloscKol = 4;
+//                pojazd.nazwa = "Traktor";
+//                pojazdList.add(pojazd);
+//
+//                pojazd = new Pojazd();
+//                pojazd.iloscKol = 2;
+//                pojazd.nazwa = "Rower";
+//                pojazd.kolor = KolorEnum.CZERMONY;
+//                pojazdList.add(pojazd);
+//
+//                /* Wyświetlenie moetody toString() na objekcie pojazd
+//                 * Można tą metodę nadpisać, w kolejnym przykładzie pokżę jak, więc proszę to zapamiętać */
+//                System.out.println(pojazd.toString()); // Pojazd@7229724f (wyświetla adres objektu pojazd)
+//
+//                for (Pojazd p : pojazdList) {
+//                System.out.println(p.nazwa + " - " + p.iloscKol + " - " + p.kolor);
+//                }
